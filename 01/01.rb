37baedfdc9b6601e98aeb49567096fc4e7586870
Bharat Agarwal <bagarwal@atlassian.com>
2020-12-01 23:28:02 +0530

    Improving Ruby syntax

--- 01/01.rb
+++ 01/01.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # loop through till right pointer reaches end
 # 	return pair if sum is found
 # increment left_pointer and right_pointer by 1
@@ -6,31 +8,28 @@
 # stop if left pointer is at penultimate and right pointer is at the end, and the sum hasnt been found
 
 def pair_adding_upto_2020(array)
-	left_pointer = 0
-	right_pointer = 1
+  left_pointer = 0
+  right_pointer = 1
 
-	while (left_pointer != array.length - 1 &&  right_pointer != array.length) do
+  while left_pointer != array.length - 1 && right_pointer != array.length
+    (right_pointer...array.length).each do |right|
+      sum = array[left_pointer] + array[right]
 
-		(right_pointer...array.length).each do |right|
-			sum = array[left_pointer] + array[right] 
+      p "sum: #{sum}"
+      p "left_pointer: #{left_pointer}"
+      p "value at left_pointer: #{array[left_pointer]}"
+      p "right_pointer: #{right}"
+      p "value at right_pointer: #{array[right]}"
+      p '-------'
 
-			p "sum: #{sum}"
-			p "left_pointer: #{left_pointer}"
-			p "value at left_pointer: #{array[left_pointer]}"
-			p "right_pointer: #{right}"
-			p "value at right_pointer: #{array[right]}"
-			p "-------"
+      return [array[left_pointer], array[right]] if sum == 2020
+    end
 
-			if sum == 2020
-				return [array[left_pointer], array[right]]
-			end
-		end
+    left_pointer += 1
+    right_pointer += 1
+  end
 
-		left_pointer += 1
-		right_pointer += 1
-	end
-
-	return nil
+  nil
 end
 
 input = File.read('input.txt', chomp: true).split("\n").map(&:to_i)
--- 01/02.rb
+++ 01/02.rb
@@ -1,28 +1,24 @@
+# frozen_string_literal: true
+
 def trio_adding_upto_2020(array)
-	left_pointer = 0
-	middle_pointer = 1
-	right_pointer = 2
+  left_pointer = 0
+  middle_pointer = 1
+  right_pointer = 2
 
-	while (left_pointer != array.length - 2 &&  middle_pointer != array.length - 1 && right_pointer != array.length) do
+  while (left_pointer != array.length - 2) && (middle_pointer != array.length - 1) && (right_pointer != array.length)
+    (middle_pointer...array.length).each do |middle|
+      (right_pointer...array.length).each do |right|
+        sum = array[left_pointer] + array[middle] + array[right]
+        return [array[left_pointer], array[middle], array[right]] if sum == 2020
+      end
+    end
 
-		(middle_pointer...array.length).each do |middle|
-			(right_pointer...array.length).each do |right|
+    left_pointer += 1
+    middle_pointer += 1
+    right_pointer += 1
+  end
 
-				sum = array[left_pointer] + array[middle] + array[right]
-				
-				if sum == 2020
-					return [array[left_pointer], array[middle], array[right]]
-
-				end
-			end
-		end
-
-		left_pointer += 1
-		middle_pointer += 1
-		right_pointer += 1
-	end
-
-	return nil
+  nil
 end
 
 input = File.read('input.txt', chomp: true).split("\n").map(&:to_i)
